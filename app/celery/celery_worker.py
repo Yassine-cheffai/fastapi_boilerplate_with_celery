c16c599c4f44e317f296bc27176c486e7decff4b
Yassine-cheffai <yacincheffai@yahoo.com>
2022-03-19 17:15:49 +0100

    use docker env variable only

--- app/celery/celery_worker.py
+++ app/celery/celery_worker.py
@@ -1,13 +1,18 @@
 import os
 import time
 from celery import Celery
-from dotenv import load_dotenv
 
-load_dotenv(".env")
+
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER_URL")
+CELERY_BROKER_URL = os.environ.get("CELERY_RESULT_BACKEND")
+if not CELERY_BROKER_URL or not CELERY_RESULT_BACKEND:
+    print("issues with the celery env variables")
 
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
+
+celery.conf.broker_url = CELERY_BROKER_URL
+celery.conf.result_backend = CELERY_RESULT_BACKEND
+
 
 @celery.task(name="create_task")
 def create_task(delay_amount, x, y):
